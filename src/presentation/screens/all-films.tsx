99817b38be20f95da529416b2c2108fe485e0272
jokswer <jokswer@gmail.com>
2021-04-21 23:22:17 +0700

    change screens

--- src/presentation/screens/all-films.tsx
+++ src/presentation/screens/all-films.tsx
@@ -1,16 +1,89 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {FlatList, StyleSheet} from 'react-native';
+import BottomSheet from 'reanimated-bottom-sheet';
+import {useDispatch, useSelector} from 'react-redux';
+
+import {IFilm} from '../../@types';
+import {ADD_TO_FAVORITES, colors} from '../../constants';
+import {getFilmsThunk, IFilmState} from '../../reducers/films';
+import {FilmCard, FilmModal, modalHeight, Loading} from '../components';
+
+const keyExtractor = (item: IFilm) => item.id;
 
 export const AllFilms = () => {
+  const sheetRef = React.useRef<BottomSheet>(null);
+  const [selectedFilm, setSelectedFilm] = React.useState<IFilm>();
+
+  const dispatch = useDispatch();
+  const films = useSelector(
+    (state: {filmsStore: IFilmState}) => state.filmsStore.films,
+  );
+  const pending = useSelector(
+    (state: {filmsStore: IFilmState}) => state.filmsStore.pending,
+  );
+
+  React.useEffect(() => {
+    dispatch(getFilmsThunk);
+  }, []);
+
+  const renderCard = React.useCallback(
+    ({item}: {item: IFilm}) => (
+      <FilmCard
+        film={item}
+        favoritesButtonText="Добавить в избранное"
+        onPressCard={() => {
+          setSelectedFilm(item);
+          sheetRef.current?.snapTo(0);
+        }}
+        onPressToFavorites={() => {
+          dispatch({type: ADD_TO_FAVORITES, film: item});
+        }}
+      />
+    ),
+    [],
+  );
+
+  const renderModal = React.useCallback(
+    () => <FilmModal film={selectedFilm} />,
+    [selectedFilm],
+  );
+
+  if (pending) {
+    return <Loading />;
+  }
+
   return (
-    <View>
-      <Text>First</Text>
-    </View>
+    <>
+      <FlatList
+        keyExtractor={keyExtractor}
+        style={styles.list}
+        data={films}
+        renderItem={renderCard}
+      />
+      <BottomSheet
+        initialSnap={1}
+        ref={sheetRef}
+        snapPoints={[modalHeight, 0]}
+        borderRadius={10}
+        renderContent={renderModal}
+      />
+    </>
   );
 };
 
 AllFilms.options = {
+  topBar: {
+    title: {
+      text: 'All films',
+    },
+  },
   bottomTab: {
-    text: 'Home',
+    text: 'All films',
   },
 };
+
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor: colors.bg,
+  },
+});
--- src/presentation/screens/favorites.tsx
+++ src/presentation/screens/favorites.tsx
@@ -1,16 +1,61 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {FlatList, StyleSheet, Text} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+
+import {IFilm} from '../../@types';
+import {colors, DELETE_FROM_FAVORITES} from '../../constants';
+import {IFavoritesState} from '../../reducers/favorites';
+import {FilmCard} from '../components';
+
+const keyExtractor = (item: IFilm) => item.id;
 
 export const Favorites = () => {
+  const films = useSelector(
+    (state: {favoritesStore: IFavoritesState}) => state.favoritesStore.films,
+  );
+  const dispatch = useDispatch();
+
+  const renderCard = React.useCallback(
+    ({item}: {item: IFilm}) => (
+      <FilmCard
+        favoritesButtonText="Удалить из избранного"
+        film={item}
+        onPressToFavorites={() => {
+          dispatch({type: DELETE_FROM_FAVORITES, id: item.id});
+        }}
+      />
+    ),
+    [],
+  );
+
   return (
-    <View>
-      <Text>Second</Text>
-    </View>
+    <FlatList
+      keyExtractor={keyExtractor}
+      style={styles.list}
+      data={films}
+      renderItem={renderCard}
+      ListEmptyComponent={<Text style={styles.emptyText}>Ничего нет</Text>}
+    />
   );
 };
 
 Favorites.options = {
+  topBar: {
+    title: {
+      text: 'Favorites',
+    },
+  },
   bottomTab: {
-    text: 'Home1',
+    text: 'Favorites',
   },
 };
+
+const styles = StyleSheet.create({
+  list: {
+    backgroundColor: colors.bg,
+  },
+  emptyText: {
+    fontSize: 14,
+    textAlign: 'center',
+  },
+});
