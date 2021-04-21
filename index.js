c1e78479350653f7794d973348fba1d560d8267e
jokswer <jokswer@gmail.com>
2021-04-21 23:22:28 +0700

    fix code style

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-import { Navigation } from 'react-native-navigation';
+import {Navigation} from 'react-native-navigation';
 
-import { ScreenNames } from './src/navigation';
-import { App } from './src/App';
+import {ScreenNames} from './src/navigation';
+import {App} from './src/App';
 
 App();
 
 Navigation.events().registerAppLaunchedListener(() => {
-    Navigation.setRoot({
-        root: {
-            bottomTabs: {
-                children: [
-                    {
-                        stack: {
-                            children: [
-                                {
-                                    component: {
-                                        name: ScreenNames.ALL_FILMS
-                                    }
-                                },
-                            ]
-                        }
-                    },
-                    {
-                        stack: {
-                            children: [
-                                {
-                                    component: {
-                                        name: ScreenNames.FAVORITES
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-    })
+  Navigation.setRoot({
+    root: {
+      bottomTabs: {
+        children: [
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: ScreenNames.ALL_FILMS,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: ScreenNames.FAVORITES,
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  });
 });
